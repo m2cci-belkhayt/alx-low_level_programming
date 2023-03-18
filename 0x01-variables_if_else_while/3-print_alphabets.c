0d2a3346e810a01a948b78d0c478019eda63ca05
tbelkhayate <belkhayatetaha1@gmail.com>
2023-03-18 14:29:47 -0700

    else_if

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char ch_1='A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		++ch;
+	
+	}
+	while (ch_1 <= 'Z')
+        {
+                putchar(ch_1);
+                ++ch_1;
+        }
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if ((ch != 'e') && (ch != 'q'))
+		{	
+			putchar(ch);
+			++ch;
+		}
+		else 
+		{
+			++ch;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
