8e505a1be1726285c3bd858a683d90c43c7a88c5
tbelkhayate <belkhayatetaha1@gmail.com>
2023-03-18 04:33:33 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /** 
  * main function
  * Description: A short description of what the code does.
  */
+
 int main(void)
 {	
 	puts("\"Programming is like building a multilingual puzzle");
