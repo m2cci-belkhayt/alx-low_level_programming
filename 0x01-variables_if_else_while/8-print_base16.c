b0060c71d20977f9f938ff092a5a8b83dfd1147e
tbelkhayate <belkhayatetaha1@gmail.com>
2023-03-18 15:05:28 -0700

    else_if

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters except q and e
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{	
+ 	int i;	
+
+        for (i =0; i < 16; i++)
+        {
+                printf("%d", i);
+        }
+        putchar('\n');
+
+        return (0);
+}
