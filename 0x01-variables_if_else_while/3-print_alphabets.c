6ad6cc578fce8e22071e56e2357dede842b77521
tbelkhayate <belkhayatetaha1@gmail.com>
2023-03-18 14:51:10 -0700

    else_if

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,19 +11,18 @@
 int main(void)
 {
 	char ch = 'a';
-	char ch_1='A';
+	char ch_1 = 'A';
 
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		++ch;
-	
 	}
 	while (ch_1 <= 'Z')
-        {
-                putchar(ch_1);
-                ++ch_1;
-        }
+	{
+		putchar(ch_1);
+		++ch_1;
+	}
 	putchar('\n');
 
 	return (0);
