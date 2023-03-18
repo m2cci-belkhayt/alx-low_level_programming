7c883261b3c068cd22688193c5d6580850655303
tbelkhayate <belkhayatetaha1@gmail.com>
2023-03-18 13:23:46 -0700

    else_if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,6 +2,11 @@
 #include <time.h>
 #include<stdio.h>
 /* more headers goes there */
+/**
+ * main: this is th main function
+ *
+ * Return: 0
+ */
 
 /* betty style doc for function main goes there */
 int main(void)
