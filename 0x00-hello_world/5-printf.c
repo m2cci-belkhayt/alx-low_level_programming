8cf635d589dee090d5d114442278875375251fa1
tbelkhayate <belkhayatetaha1@gmail.com>
2023-03-18 04:51:14 -0700

    C_program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/** 
+ *main function
+ *
+ *Description: A short description of what the code does.
+ *
+ *Return 0
+ */
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
