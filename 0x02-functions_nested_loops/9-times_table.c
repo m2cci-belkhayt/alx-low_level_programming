0b4eea813338b5a7c8c23652d835ddaee3700ed9
tbelkhayate <belkhayatetaha1@gmail.com>
2023-03-21 16:10:47 -0700

    hello

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * times_table - prints times table
+ * Return:void
+ */
+
+void times_table(void)
+{
+	int i,j;
+	
+	for (i = 0; i < 9; i++)
+	{	
+		for (j = 0; j < 9; j++)
+		{
+			_putchar('0' + i*j);
+			_putchar(',');
+		}
+		_putchar('\n');
+	}
+	return;
+}
