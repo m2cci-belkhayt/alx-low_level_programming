ccc6fe9cfca5c4ceff62be65956544e292a90aeb
tbelkhayate <belkhayatetaha1@gmail.com>
2023-03-21 15:15:20 -0700

    functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet_x10(void)
+{	
+	int i=0;
+	for ( i=0;i<10;i++ )
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+
+		}
+		_putchar('\n');
+	}
+}
