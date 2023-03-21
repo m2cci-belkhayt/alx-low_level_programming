227c59d23c92708a16a86ad7bcd46bf4454a7e4b
tbelkhayate <belkhayatetaha1@gmail.com>
2023-03-21 15:45:12 -0700

    functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _abs- return the absolite numers
+ * @n : number to check
+ * Return: numberabs
+ */
+
+int int _abs(int)
+{
+	int test;
+
+	if (n > 0)
+	{
+		test = n;
+	}
+	else if (n == 0)
+	{
+		test = 0;
+	}
+	else
+	{
+		test = -n;
+	}
+
+	return (test);
+}
