5a66d78d7834c2f4e3456fc44ef04fdc0c0076dd
tbelkhayate <belkhayatetaha1@gmail.com>
2023-03-23 05:40:09 -0700

    third_task

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,5 @@
+lines (8 sloc)  229 Bytes
+
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -6,6 +8,7 @@
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 
-#endif /* MAIN_H */
+#endif
