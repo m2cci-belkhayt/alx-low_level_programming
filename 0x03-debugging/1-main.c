945f4b35a8365ad354b56fbd3b628411d2a8cc76
tbelkhayate <belkhayatetaha1@gmail.com>
2023-03-23 04:35:05 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,10 +12,10 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 	i = 0;
 
-	while (i < 10)/* this chunk gives the infinit loop */
+	/* while (i < 10) this chunk gives the infinit loop 
 	{
 		putchar(i);
-	}
+	} */
 
 	printf("Infinite loop avoided! \\o/\n");
 
