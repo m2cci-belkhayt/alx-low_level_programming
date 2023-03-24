dec097daa68c73471ddb1d6047551d9d99fd5dd2
tbelkhayate <belkhayatetaha1@gmail.com>
2023-03-24 15:04:13 -0700

    sisth exo

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - nmbr fct
+ * Bwave ICT file
+ * Return: no return
+ */
+
+void more_numbers(int n)
+{
+
+	int i = 0;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	else
+		_putchar('\n');
+}
