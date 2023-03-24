19a52326cbfcf96c1fa38c77ce591068c87106eb
tbelkhayate <belkhayatetaha1@gmail.com>
2023-03-24 03:21:16 -0700

    first task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - verify that we have an uppercase
+ *
+ * @c: character to check
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+
+}
