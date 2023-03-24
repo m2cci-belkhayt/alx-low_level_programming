5a8a09730dd97db68bdf39cf6d5d965feb9199db
tbelkhayate <belkhayatetaha1@gmail.com>
2023-03-24 03:34:18 -0700

    second task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplication
+ * @a and @b: two integeres or even real numbers
+ *
+ * Return: multiplication
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c=a*b;
+
+	return(c);
+}
