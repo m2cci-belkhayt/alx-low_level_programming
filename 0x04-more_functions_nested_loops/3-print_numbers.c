293ff7c70b4cd1862cd49c25bc7a57d70da9295c
tbelkhayate <belkhayatetaha1@gmail.com>
2023-03-24 14:23:14 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - from 0 to 9 check the code
+ *
+ * Return: Always void.
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i)
+	}
+	_putchar('\n')
+}
