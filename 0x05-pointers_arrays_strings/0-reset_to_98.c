f9d84d407c5b672b2ed25a578da5ed3074cbd9c4
tbelkhayate <belkhayatetaha1@gmail.com>
2023-03-27 15:37:07 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * main - point to something
+ * reset_to_98 - point to something
  *
+ * @n: int to check
+ * 
  * Return: Always nothing.
  */
 
 void reset_to_98(int *n)
-{	
-	*n=98;
+{
+	*n = 98;
 }
