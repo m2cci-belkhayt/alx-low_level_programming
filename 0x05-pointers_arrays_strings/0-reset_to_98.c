935b2434f27d8c5eebbc28e626f0906b5adc3840
tbelkhayate <belkhayatetaha1@gmail.com>
2023-03-27 15:32:44 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - point to something
+ *
+ * Return: Always nothing.
+ */
+
+void reset_to_98(int *n)
+{	
+	*n=98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+we are deaing with pointers now
