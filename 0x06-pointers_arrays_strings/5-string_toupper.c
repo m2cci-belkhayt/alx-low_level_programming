fbc1bffdbc79ce1e3fa770573a1a3a2eac8a2e4c
tbelkhayate <belkhayatetaha1@gmail.com>
2023-03-31 14:56:29 -0700

    hy

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,15 +3,13 @@
  * string_toupper - change lowercase to uppercase
  *
  * @s:string
- * 
- * Return:char
+ * Return: char
  */
 char *string_toupper(char *s)
 {
-
 	int i;
 
-i = 0;
+	i = 0;
 	while (*(s + i))
 	{
 		if (*(s + i) >= 'a' && *(s + i) <= 'z')
