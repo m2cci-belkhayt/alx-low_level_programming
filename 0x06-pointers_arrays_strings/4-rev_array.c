d352ac0b1e7176aea8046d7463909b9238e4d217
tbelkhayate <belkhayatetaha1@gmail.com>
2023-03-31 15:06:07 -0700

    hy

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * reverse_array - reverse an array
+ * reverse_array - reverse array
  * @a:array
- * @n:integer 
+ * @n:integer
  * Return:void
  */
 void reverse_array(int *a, int n)
@@ -11,8 +11,8 @@ int i, c;
 
 for (i = 0; (i < (n - 1) / 2); i++)
 	{
-		c = a[i];
-		a[i] = a[n - 1 - i];
-		a[n - 1 - i] = c;
+	c = a[i];
+	a[i] = a[n - 1 - i];
+	a[n - 1 - i] = c;
 	}
 }
