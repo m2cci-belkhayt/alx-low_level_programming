488896701efb0c99e99c26a8ccc7322faee47284
tbelkhayate <belkhayatetaha1@gmail.com>
2023-03-31 14:45:12 -0700

    all for one

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,24 +1,26 @@
 #include "main.h"
 
 /**
- * _strncat - concatenate words of length n
- * @dest which is pointer of the string
- * @src: same thing a string pointer
- * @n: is a lrngth
- * Return: Always string of length n.
+ * _strncat - concatenates two strings
+ *
+ * @src: The source of strings
+ * @dest: The destination of the string
+ * @n: The length of int
+ *
+ * Return: pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	for  (i = 0; dest[i] != '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++)
 	{
 		continue;
 	}
-	for (j = 0; src != '\0' && j <= n; j++)
+	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
 		dest[i + j] = src[j];
 	}
-	des[i + j] = '\0';
-	return(dest)
+	dest[i + j] = '\0';
+	return (dest);
 }
