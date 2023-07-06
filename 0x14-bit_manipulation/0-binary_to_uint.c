0d90c81c808da102d12f463faae2c29f45b63937
tbelkhayate <belkhayatetaha1@gmail.com>
2023-07-06 16:00:24 -0700

    changes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,31 +7,27 @@
  *
  * Return: unsigned int.
  */
-unsigned int binary_to_uint(const char *b)
+unsigned int binary_to_uint(const char *b) 
 {
-	unsigned int ui;
-	int len, base_two;
+    unsigned int result = 0;
+    int len = 0;
+    int i;
+    int base = 1;
 
-	if (!b)
-		return (0);
+    while (b[len] != '\0') {
+        len++;
+    }
 
-	ui = 0;
+ 
+    for (i = len - 1; i >= 0; i--) {
+        if (b[i] != '0' && b[i] != '1') {
+            return 0;
+        }
+        if (b[i] == '1') {
+            result += base;
+        }
+        base *= 2;
+    }
 
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
-	{
-		if (b[len] != '0' && b[len] != '1')
-		{
-			return (0);
-		}
-
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
-	}
-
-	return (ui);
+    return result;
 }
