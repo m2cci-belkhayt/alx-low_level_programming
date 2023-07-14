6d69213c73ae09d06a86fe03c505128438b7fd2f
tbelkhayate <belkhayatetaha1@gmail.com>
2023-07-14 08:48:52 -0700

    last tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,28 +10,22 @@ int create_file(const char *filename, char *text_content)
 {
 	int file, writes, len = 0;
 
-	/* check valid filename */
 	if (filename == NULL)
 		return (-1);
 
-	/* create empty file */
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
 
-	/* if text_content is NULL */
 	if (!text_content)
 	{
 		close(file);
 		return (1);
 	}
 
-	/* get the length of text_content */
 	while (text_content[len])
 		len++;
 
-
-	/* write into file */
 	writes = write(file, text_content, len);
 	if (writes == -1)
 		return (-1);
