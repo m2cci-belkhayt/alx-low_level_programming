3c0d4f10641566ee162fbe847f4f3c6868023218
tbelkhayate <belkhayatetaha1@gmail.com>
2023-07-14 08:37:37 -0700

    second

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,37 @@
 #include "main.h"
 
 /**
- * append_text_to_file - appends a file
- * @filename: file to be appended
- * @text_content: text to be added at the end of file
- * Return: 1 on success, -1 on failure
+ * create_file - creates a file
+ * @filename: name of file to be created
+ * @text_content: string to be written to file
+ * Return: 1 on success, -1 on fail
  */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int file, writes, len = 0;
 
+	/* check valid filename */
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_APPEND | O_RDWR, 0664);
+	/* create empty file */
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
 
+	/* if text_content is NULL */
 	if (!text_content)
 	{
 		close(file);
 		return (1);
 	}
 
+	/* get the length of text_content */
 	while (text_content[len])
 		len++;
 
+
+	/* write into file */
 	writes = write(file, text_content, len);
 	if (writes == -1)
 		return (-1);
