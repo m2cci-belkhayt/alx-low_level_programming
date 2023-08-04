03e41b4c9c79a266078ab795ed8c0b2b0d7b5eb1
tbelkhayate <belkhayatetaha1@gmail.com>
2023-08-04 13:14:02 -0700

    task again

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,10 +14,10 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 	int base = 1;
 	int length = 0;
-	
-	if (b == NULL) 
+
+	if (b == NULL)
 	{
-        	return (0);
+		return (0);
 	}
 	for (i = 0; b[i] != '\0'; i++)
 	{
