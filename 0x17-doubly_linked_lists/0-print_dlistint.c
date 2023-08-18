6ce8297cc5cd30834f68e95907b26010af35ebe1
tbelkhayate <belkhayatetaha1@gmail.com>
2023-08-18 23:20:43 +0200

    task 1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 /**
- * * print_dlistint - function
- * * @h: string to print
- * *
- * * Description: Function to print all elements in dlistint_t list
- * * Return: number of nodes
- * */
+ * print_dlistint - function
+ * @h: pointer that points to the head
+ *
+ * Description: Function to print all elements in dlistint_t list
+ * Return: number of nodes
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	int elements = 0;
