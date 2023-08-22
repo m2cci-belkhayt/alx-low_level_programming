77dc2cd517b5e87b94bc54930162cb8f922e0f03
tbelkhayate <belkhayatetaha1@gmail.com>
2023-08-22 09:33:12 +0200

    task 100

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,6 +1,23 @@
 #include<stdio.h>
 
-int addition(int a, int b)
+int add(int a, int b)
 {
-	return (a+b);
+	return (a + b);
+}
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+int mul(int a, int b)
+{
+	return (a * b);
+}
+int div(int a, int b)
+{
+	return (a / b);
+}
+int mod(int a, int b)
+{
+	return (a % b);
 }
